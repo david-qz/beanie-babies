b9e54df3c377ee20449d21d1d9bce1eb37d8f140
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-21 18:19:52 -0700

    Implement BeanBabyList component

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 // import services and utilities
+import { getBeanieBabies } from './services/beanie-babies-service.js';
 
 // import component creators
+import createBeanieBabyList from './components/BeanieBabyList.js';
 
 // declare state variables
+let beanieBabies = [];
 
 // write handler functions
+async function handlePageLoad() {
+    beanieBabies = await getBeanieBabies();
+    display();
+}
 
-// Create each component: 
+// Create each component:
 // - pass in the root element via querySelector
-// - pass any needed handler functions as properties of an actions object 
+// - pass any needed handler functions as properties of an actions object
+const beanieBabyList = createBeanieBabyList(document.querySelector('#beanie-baby-list'));
 
 // Roll-up display function that renders (calls with state) each component
 function display() {
-    // Call each component passing in props that are the pieces of state this component needs
+    beanieBabyList({ beanieBabies });
 }
 
-// Call display on page load
-display();
+// Page load actions
+handlePageLoad();
---
+++ components/BeanieBabyList.js
@@ -0,0 +1,33 @@
+export default function createBeanieBabyList(root) {
+
+    return ({ beanieBabies }) => {
+        root.innerHTML = '';
+
+        for (const beanieBaby of beanieBabies) {
+            const element = BeanieBabyCard({ beanieBaby });
+            root.append(element);
+        }
+    };
+}
+
+export function BeanieBabyCard({ beanieBaby }) {
+    const li = document.createElement('li');
+    li.classList.add('beanie-baby-card');
+
+    const a = document.createElement('a');
+    const params = new URLSearchParams();
+    params.set('id', beanieBaby.id);
+    a.href = `detail/?${params.toString()}`;
+
+    const img = document.createElement('img');
+    img.src = beanieBaby.image;
+    img.alt = beanieBaby.title;
+
+    const p = document.createElement('p');
+    p.textContent = beanieBaby.title;
+
+    li.append(img, p);
+    a.append(li);
+
+    return a;
+}
