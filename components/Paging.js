b7c6dc19e1bbdcc4c84dd5b5d46eed169d84577f
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-24 10:47:02 -0700

    Implement Paging component

---
+++ components/Paging.js
@@ -0,0 +1,30 @@
+export default function createPaging(root, { handlePaging }) {
+    const pageSizeSelect = root.querySelector('select');
+    const pageLocationSpan = root.querySelector('span');
+    const [firstButton, prevButton, nextButton, lastButton] = root.querySelectorAll('button');
+
+    pageSizeSelect.addEventListener('change', () => {
+        handlePaging(null, pageSizeSelect.value);
+    });
+
+    firstButton.addEventListener('click', () => {
+        handlePaging('first', pageSizeSelect.value);
+    });
+
+    prevButton.addEventListener('click', () => {
+        handlePaging('prev', pageSizeSelect.value);
+    });
+
+    nextButton.addEventListener('click', () => {
+        handlePaging('next', pageSizeSelect.value);
+    });
+
+    lastButton.addEventListener('click', () => {
+        handlePaging('last', pageSizeSelect.value);
+    });
+
+    return ({ pageSize, pageNumber, totalPages }) => {
+        pageSizeSelect.value = pageSize;
+        pageLocationSpan.textContent = `Page ${pageNumber + 1} of ${totalPages}`;
+    };
+}
